a426f7a774a9b3336a5fdf549e3c56cf8df5db3f
Jacobo Vazquez <vazquezmjacobo@gmail.com>
2024-06-19 17:37:01 -0400

    Initial Commit

---
+++ testProject/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  testProject
+//
+//  Created by Jacobo Vazquez on 6/19/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
